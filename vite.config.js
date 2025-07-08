78a11489ecc91e80eef6d653eb7f3166939e3e9b
Nikolay <nikolay.zatonskiy>
2025-07-08 18:17:51 +0300

    fix gh-pages #6

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: "/pet-clinicial-portal/",
   plugins: [react(), tailwindcss()],
 });
