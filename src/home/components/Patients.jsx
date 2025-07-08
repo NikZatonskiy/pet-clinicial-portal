f83a3a945709e2b389914dfe8dc6b2b2acf1831a
Nikolay <nikolay.zatonskiy>
2025-07-08 17:29:30 +0300

    added gh-pages

--- src/home/components/Patients.jsx
+++ src/home/components/Patients.jsx
@@ -30,7 +30,18 @@ function Patients() {
       <BarChart width={300} height={250} data={data}>
         <XAxis dataKey="name" stroke="#8884d8" />
         <YAxis dataKey="total" unit="%" tickCount={3} />
-        <Legend width={100} />
+        {/* <Tooltip wrapperStyle={{ width: 100, backgroundColor: "#ccc" }} /> */}
+        <Legend
+          width={100}
+          // wrapperStyle={{
+          //   top: 40,
+          //   right: 20,
+          //   backgroundColor: "#f5f5f5",
+          //   border: "1px solid #d5d5d5",
+          //   borderRadius: 3,
+          //   lineHeight: "40px",
+          // }}
+        />
         <CartesianGrid stroke="#ccc" strokeDasharray="5 5" vertical={false} />
         <Bar dataKey="amount" fill="#8884d8" barSize={60} />
       </BarChart>
