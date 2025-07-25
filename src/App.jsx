4b0e24109225da9739363d243cb9344832a49ee7
Nikolay <nikolay.zatonskiy>
2025-07-26 02:11:16 +0300

    added redirect from any other url to home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import "./App.css";
-import { Routes, Route } from "react-router";
+import { Routes, Route, Navigate } from "react-router";
 import Home from "./components/home/Home";
 import NavBar from "./components/navbar/NavBar";
 import Patients from "./components/Patients/Patients";
@@ -9,6 +9,10 @@ function App() {
     <div className="app flex">
       <NavBar />
       <Routes>
+        <Route
+          path="*"
+          element={<Navigate to="/pet-clinicial-portal/home/" replace={true} />}
+        />
         <Route path="/pet-clinicial-portal/home/" element={<Home />} />
         <Route path="/pet-clinicial-portal/patients/" element={<Patients />} />
       </Routes>
